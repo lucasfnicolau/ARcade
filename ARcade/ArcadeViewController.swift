c68281fdcc1cdf9044e138f7f1e35d09d2a1b325
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-02-20 17:53:39 -0300

    Tudo finalmente funciona.

--- ARcade/ArcadeViewController.swift
+++ ARcade/ArcadeViewController.swift
@@ -24,12 +24,11 @@ class ArcadeViewController: UIViewController, ARSessionDelegate {
     }
     var currentState: GameState = .arcade
     var score = 0
+    var remainingTime = 60
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
-//        arView.addGestureRecognizer(tap)
 
         arView.session.delegate = self
 
@@ -46,130 +45,24 @@ class ArcadeViewController: UIViewController, ARSessionDelegate {
         case arcadeAnchor.blueButton,
              arcadeAnchor.greenButton:
             currentState = .alienXUSLevel0
-//            levelsAnchors[0].bullet?.scale = SIMD3(0, 0, 0)
-            set(text: "Score: 0", for: levelsAnchors[0].scoreLabel)
-//            levelsAnchors[0].actions.fakesDeath.onAction = fakesDeath(_:)
-//            levelsAnchors[0].actions.realDeath.onAction = realDeath(_:)
+            arView.scene.anchors.append(levelsAnchors[0])
+            startTimer()
         default:
             break
         }
     }
 
-    func fakesDeath(_ entity: Entity?) {
-        set(text: "Game Over", for: levelsAnchors[0].scoreLabel)
-    }
+    func startTimer() {
+        set(text: "\(remainingTime)s", for: levelsAnchors[0].timerLabel)
+        timer = Timer(timeInterval: 1, repeats: true, block: { (timer) in
+            self.remainingTime -= 1
+            set(text: "\(self.remainingTime)s", for: self.levelsAnchors[0].timerLabel)
 
-    func realDeath(_ entity: Entity?) {
-        set(text: "\(score + 10)", for: levelsAnchors[0].scoreLabel)
-    }
-
-    @objc
-    func handleTap(recognizer: UITapGestureRecognizer) {
-        let location = recognizer.location(in: arView)
-
-        // Attempt to find a 3D location on a horizontal surface underneath the user's touch location.
-        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
-//        if let firstResult = results.first {
-//            // Add an ARAnchor at the touch location with a special name you check later in `session(_:didAdd:)`.
-//            let anchor = ARAnchor(name: Anchor.placingObject.rawValue, transform: firstResult.worldTransform)
-//            arView.session.add(anchor: anchor)
-//
-//        } else {
-//            print("Warning: Object placement failed.")
-//        }
-
-        let cameraAnchor = ARAnchor(name: Anchor.camera.rawValue, transform: session.currentFrame!.camera.transform)
-        session.add(anchor: cameraAnchor)
-    }
-
-    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-        for anchor in anchors {
-            if anchor.name == Anchor.camera.rawValue {
-                let anchorEntity = AnchorEntity(anchor: anchor)
-                //                 potatoAnchor.potato?.components.set(CollisionComponent(shapes: [.generateCapsule(height: 0.15, radius: 0.03)], mode: .trigger, filter: .sensor))
-
-                //                arView.scene.subscribe(to: CollisionEvents.Began.self) { (event) in
-                //                    print("Colidiu \(event.entityA) com \(event.entityB)")
-                //                }.store(in: &collidedObjects)
-
-                //                if let potato = (potatoAnchor.potato as? Entity & HasPhysics) {
-                //                    potato.physicsBody?.isContinuousCollisionDetectionEnabled = true
-                //                }
-
-//                let radius: Float = 0.5
-//                let bullet = ModelEntity(mesh: MeshResource.generateSphere(
-//                    radius: radius),
-//                                         materials: [SimpleMaterial(color: .black, isMetallic: true)]
-//                )
-
-//                guard let bullet = levelsAnchors[0].bullet?.clone(recursive: true) else {
-//                    return
-//                }
-//                bullet.scale = SIMD3(1, 1, 1)
-//
-//                //Setando a ancora dela
-//                anchorEntity.addChild(bullet)
-//                bullet.position = [0, 0, -1]
-
-                //Colocando a ancora com a bolinha na cena
-                arView.scene.addAnchor(anchorEntity)
-
-//                levelsAnchors[0].notifications.shoot.post()
-//                bullet.addForce(SIMD3(20, 20, 20), relativeTo: bullet)
+            if self.remainingTime <= 0 {
+                //
             }
-        }
-    }
+        })
 
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        guard error is ARError else { return }
-
-        let errorWithInfo = error as NSError
-        let messages = [
-            errorWithInfo.localizedDescription,
-            errorWithInfo.localizedFailureReason,
-            errorWithInfo.localizedRecoverySuggestion
-        ]
-
-        // Remove optional error messages.
-        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
-
-        DispatchQueue.main.async {
-            // Present the error that occurred.
-            let alertController = UIAlertController(title: "The AR session failed.", message: errorMessage, preferredStyle: .alert)
-            let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
-                alertController.dismiss(animated: true, completion: nil)
-                self.resetTracking()
-            }
-            alertController.addAction(restartAction)
-            self.present(alertController, animated: true, completion: nil)
-        }
-    }
-
-    func resetTracking() {
-        guard let configuration = arView.session.configuration else { print("A configuration is required"); return }
-        arView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
-    }
-}
-
-class Bullet: Entity, HasModel, HasAnchoring, HasCollision {
-
-    required init(color: UIColor) {
-        super.init()
-        self.components[ModelComponent] = ModelComponent(
-            mesh: .generateSphere(radius: 1),
-            materials: [SimpleMaterial(
-                color: color,
-                isMetallic: false)
-            ]
-        )
-    }
-
-    convenience init(color: UIColor, position: SIMD3<Float>) {
-        self.init(color: color)
-        self.position = position
-    }
-
-    required init() {
-        fatalError("init() has not been implemented")
+        timer?.fire()
     }
 }
