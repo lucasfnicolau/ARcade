21c060acbe744c427554540b7ca068c859ae975e
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-02-21 00:25:46 -0300

    Finalização do jogo.

--- ARcade/ArcadeViewController.swift
+++ ARcade/ArcadeViewController.swift
@@ -12,19 +12,21 @@ import Combine
 
 class ArcadeViewController: UIViewController, ARSessionDelegate {
     
-    @IBOutlet var arView: ARView!
+    @IBOutlet var arView: ARcadeView!
     var configuration: ARWorldTrackingConfiguration?
     var anyCancellables: [AnyCancellable] = []
-    let arcadeAnchor = try! Experience.loadArcade()
-    var levelsAnchors = [
+    var arcadeAnchor: Experience.Arcade = try! Experience.loadArcade()
+    var levelsAnchors: [Experience.Level0] = [
         try! Experience.loadLevel0()
     ]
     var session: ARSession{
         return arView.session
     }
     var currentState: GameState = .arcade
-    var score = 0
     var remainingTime = 60
+    var kills = 0
+    var tutorialView: UIView?
+    var tutorialTextView: UITextView?
     var timer: Timer?
     
     override func viewDidLoad() {
@@ -40,12 +42,53 @@ class ArcadeViewController: UIViewController, ARSessionDelegate {
         }
     }
 
+    func showTutorial() {
+        tutorialTextView = UITextView(frame: .zero)
+
+        guard let tutorialTextView = tutorialTextView else { return }
+        tutorialTextView.translatesAutoresizingMaskIntoConstraints = false
+
+        arView.addSubview(tutorialTextView)
+
+        tutorialTextView.text = """
+        You have up to 60s to save the Earth!
+
+        Kill at least 12 aliens by tapping them twice and keep humanity alive!!!
+
+        Good luck :D
+        """
+        tutorialTextView.textColor = .yellow
+        tutorialTextView.textAlignment = .center
+        tutorialTextView.font = UIFont(name: Font.pressStart.rawValue, size: 24)!
+        tutorialTextView.isEditable = false
+        tutorialTextView.isSelectable = false
+        tutorialTextView.backgroundColor = .black
+        tutorialTextView.layer.cornerRadius = 35
+
+        NSLayoutConstraint.activate([
+            tutorialTextView.centerXAnchor.constraint(equalTo: arView.centerXAnchor),
+            tutorialTextView.centerYAnchor.constraint(equalTo: arView.centerYAnchor),
+            tutorialTextView.widthAnchor.constraint(equalTo: arView.widthAnchor, multiplier: 0.8),
+            tutorialTextView.heightAnchor.constraint(equalToConstant: 340),
+        ])
+
+        UIView.animate(withDuration: 3, delay: 5, animations: {
+            tutorialTextView.alpha = 0
+        }) { (_) in
+            tutorialTextView.removeFromSuperview()
+        }
+    }
+
     func changeGameState(basedOn entity: Entity?) {
         switch entity {
         case arcadeAnchor.blueButton,
              arcadeAnchor.greenButton:
+            showTutorial()
             currentState = .alienXUSLevel0
             arView.scene.anchors.append(levelsAnchors[0])
+            levelsAnchors[0].actions.allActions.forEach { (action) in
+                action.onAction = handleTapOnEntity(_:)
+            }
             startTimer()
         default:
             break
@@ -53,16 +96,23 @@ class ArcadeViewController: UIViewController, ARSessionDelegate {
     }
 
     func startTimer() {
-        set(text: "\(remainingTime)s", for: levelsAnchors[0].timerLabel)
-        timer = Timer(timeInterval: 1, repeats: true, block: { (timer) in
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
             self.remainingTime -= 1
-            set(text: "\(self.remainingTime)s", for: self.levelsAnchors[0].timerLabel)
 
             if self.remainingTime <= 0 {
-                //
+                self.levelsAnchors[0].notifications.explosion.post()
+                self.timer?.invalidate()
             }
         })
 
         timer?.fire()
+    }
+
+    func handleTapOnEntity(_ entity: Entity?) {
+        kills += 1
+
+        if kills >= 12 {
+            levelsAnchors[0].notifications.victory.post()
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ARcade
 ### This game explores the use of ARKit with Reality Composer
+
+Electronic Fantasy music by: Patrick de Arteaga (https://patrickdearteaga.com/arcade-music/)
